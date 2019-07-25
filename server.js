1025d973476aed7185b7ce92da770df2506dc395
jeffery.raimundo@medaire.com <jeff_pepper23@yahoo.com>
2019-07-24 19:58:16 -0700

    backend routes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const routes = require("./routes");
 // const apiKey = process.env.API_KEY;
 // console.log(apiKey);
 
@@ -9,13 +10,14 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("client/build"));
+app.use(routes);
 
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
+// app.get('*', function(req, res) {
+//   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+// });
 
 
-require("./routes/routes-API.js")(app);
+require("./routes/api/routes-API")(app);
 
 app.listen(PORT, () => console.log("Congratulations!! API Server is now listening on: " + PORT));
 
